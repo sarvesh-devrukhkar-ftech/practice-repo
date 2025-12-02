8bca014bc6d4e5579dd34f23b78fa39a9e3c4715
Gauri Patil <gauri.patil@fafadiatech.com>
2025-12-02 16:12:40 +0530

    added json

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def fetch_page(url):
@@ -11,34 +11,66 @@ def fetch_page(url):
     else:
         print("failed to fetched data")
 
-MAX_ITEMS = 50
 
 def parse_page(response):
-    return BeautifulSoup(response.text, "html.parser")
+    if response is None:
+        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
 
-
-def parse_next_urls(parsed_data):
-    if parsed_data is None:
+def parse_next_urls(soup):
+    if soup is None:
         return []
 
     next_urls = []
-    links = parsed_data.find_all("a", href=True)
 
-    for title in parsed_data.find_all("h2"):
-        if(title.string):
-            print("heading-----",title.get_text())
-            
     
+    links = soup.find_all("a", href=True)
 
+
+    title_tag = soup.find('meta', property="og:title")
+    if title_tag:
+        og_title = title_tag.get("content")
+    else:
+        og_title = "not found"
+
+    description_tag = soup.find('meta', property="og:description")
+    if description_tag:
+        og_description = description_tag.get("content")
+    else:
+        og_description = "not found"
+
+    image_tag = soup.find('meta', property="og:image")
+    if image_tag:
+        og_image = image_tag.get("content")
+    else:
+        og_image = "not found"
+
+    image_alt_tag = soup.find('meta', property="og:image:alt")
+    if image_alt_tag:
+        og_image_alt = image_alt_tag.get("content")
+    else:
+        og_image_alt = "not found"
+
+    print("OG Title:", og_title)
+    print("OG Description:", og_description)
+    print("OG Image URL:", og_image)
+    print("OG Image Alt Text:", og_image_alt)
+    print("---------------------------------------------------")
+
+
+    candidiates = ["#", "javascript:", "mailto:"]
     for link in links:
         href = link["href"]
 
-        if href.startswith("#"):
+        skip = False
+        for item in candidiates:
+            if href.startswith(item):
+                skip = True
+                break
+        if skip:
             continue
-        if href.startswith("javascript:"):
-            continue
-        if href.startswith("mailto:"):
-            continue
+
         if href.startswith("/"):
             href = "https://www.bbc.com" + href
 
@@ -46,25 +78,35 @@ def parse_next_urls(parsed_data):
 
     return next_urls
 
-
 frontier = [
     "https://www.bbc.com/news",
     "https://www.bbc.com/sport",
     "https://www.bbc.com/business",
 ]
-visited = {}
+visited = {} 
+results =[]
 
 while len(frontier) > 0:
     current_url = frontier.pop(0)
     if current_url in visited:
         continue
     response = fetch_page(current_url)
-    parsed_data = parse_page(response)
-    next_urls = parse_next_urls(parsed_data)
+    soup = parse_page(response)
+    next_urls = parse_next_urls(soup)
+    url = parse_next_urls
     frontier.extend(next_urls)
     visited[current_url] = ""
     print(f"Visited on : {current_url} | Found: {len(next_urls)} links")
 
 
+    results={
+    "url": next_urls,
+    # "title": og_title,
+    # "description": og_description,
+    # "image": og_image,
+    # "image_alt": og_image_alt
+      }
 
 
+    with open("crawler_test.json", "w", encoding="utf-8") as file:
+        json.dump(results, file, indent=4, ensure_ascii=False)
